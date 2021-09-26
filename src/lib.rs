22c91978a085418f51fe757f5a85f4f4b6a1aae0
Johannes Hoff <johshoff@gmail.com>
2021-09-26 10:41:27 +0200

    Get type of number

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,65 @@ pub struct Fødselsnummer {
     value: u64,
 }
 
+#[derive(PartialEq, Eq, Debug)]
 pub enum Type {
     Normal,
     DNumber,
     HNumber,
+    FHNumber,
+}
+
+/// Calculate the checksum of a number with or without an existing checksum.
+pub fn calculate_checksum(value: u64) -> Option<u64> {
+    fn check_digit(partial_sum: u64) -> Option<u64> {
+        match 11 - (partial_sum % 11) {
+            11 => Some(0),
+            10 => None,
+            n if n < 10 => Some(n),
+            _ => unreachable!("n can't be less than zero after 11 - x mod 11"),
+        }
+    }
+
+    let mut divisor = 10000000000;
+    let mut digits = Vec::with_capacity(10);
+    let mut remaining = value;
+    for _ in 0..9 {
+        // get all values except checksum
+        let digit = remaining / divisor;
+        remaining -= digit * divisor;
+        divisor /= 10;
+        digits.push(digit);
+    }
+
+    let k1 = check_digit(
+        vec![3, 7, 6, 1, 8, 9, 4, 5, 2]
+            .iter()
+            .zip(&digits)
+            .map(|(f, d)| (f * d) as u64)
+            .sum::<u64>(),
+    )?;
+    digits.push(k1);
+
+    let k2: u64 = check_digit(
+        vec![5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
+            .iter()
+            .zip(&digits)
+            .map(|(f, d)| f * d)
+            .sum::<u64>(),
+    )?;
+
+    Some(k1 * 10 + k2)
 }
 
 impl Fødselsnummer {
+    pub fn new(value: u64) -> Option<Fødselsnummer> {
+        if value < 100000000000 {
+            Some(Fødselsnummer { value })
+        } else {
+            None
+        }
+    }
+
     pub fn from_string(s: &str) -> Option<Fødselsnummer> {
         if s.len() == 11 {
             s.parse::<u64>().ok().map(|value| Fødselsnummer { value })
@@ -19,44 +71,7 @@ impl Fødselsnummer {
     }
 
     pub fn calculate_checksum(&self) -> Option<u64> {
-        fn check_digit(partial_sum: u64) -> Option<u64> {
-            match 11 - (partial_sum % 11) {
-                11 => Some(0),
-                10 => None,
-                n if n < 10 => Some(n),
-                _ => unreachable!("n can't be less than zero after 11 - x mod 11"),
-            }
-        }
-
-        let mut divisor = 10000000000;
-        let mut digits = Vec::with_capacity(10);
-        let mut remaining = self.value;
-        for _ in 0..9 {
-            // get all values except checksum
-            let digit = remaining / divisor;
-            remaining -= digit * divisor;
-            divisor /= 10;
-            digits.push(digit);
-        }
-
-        let k1 = check_digit(
-            vec![3, 7, 6, 1, 8, 9, 4, 5, 2]
-                .iter()
-                .zip(&digits)
-                .map(|(f, d)| (f * d) as u64)
-                .sum::<u64>(),
-        )?;
-        digits.push(k1);
-
-        let k2: u64 = check_digit(
-            vec![5, 4, 3, 2, 7, 6, 5, 4, 3, 2]
-                .iter()
-                .zip(&digits)
-                .map(|(f, d)| f * d)
-                .sum::<u64>(),
-        )?;
-
-        Some(k1 * 10 + k2)
+        calculate_checksum(self.value)
     }
 
     pub fn is_valid_checksum(&self) -> bool {
@@ -70,16 +85,50 @@ impl Fødselsnummer {
     }
 
     pub fn get_type(&self) -> Type {
-        unimplemented!();
+        let d0 = self.digit(0);
+        if d0 >= 8 {
+            Type::FHNumber
+        } else if d0 >= 4 {
+            Type::DNumber
+        } else if self.digit(2) >= 4 {
+            Type::HNumber
+        } else {
+            Type::Normal
+        }
+    }
+
+    fn digit(&self, i: u64) -> u64 {
+        match i {
+            0 => self.value / 10000000000,
+            1 => (self.value / 1000000000) % 10,
+            2 => (self.value / 100000000) % 10,
+            3 => (self.value / 10000000) % 10,
+            4 => (self.value / 1000000) % 10,
+            5 => (self.value / 100000) % 10,
+            6 => (self.value / 10000) % 10,
+            7 => (self.value / 1000) % 10,
+            8 => (self.value / 100) % 10,
+            9 => (self.value / 10) % 10,
+            10 => self.value % 10,
+            _ => panic!("digit() called with out of range i"),
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Fødselsnummer;
+    use crate::*;
 
     fn fs(s: &str) -> Option<Fødselsnummer> {
         Fødselsnummer::from_string(s)
+    }
+
+    // Fødselsnummer from number without checksum
+    fn fs_wo(value: u64) -> Fødselsnummer {
+        Fødselsnummer::new(
+            value * 100 + calculate_checksum(value).expect("expected valid checksum for test date"),
+        )
+        .expect("expected valid number for test data")
     }
 
     #[test]
@@ -126,5 +175,17 @@ mod tests {
         assert!(fs("21016514958").unwrap().is_valid_checksum());
 
         assert!(!fs("21016514959").unwrap().is_valid_checksum());
+    }
+
+    #[test]
+    fn get_type() {
+        assert_eq!(fs_wo(020636266).get_type(), Type::Normal);
+        assert_eq!(fs_wo(301251144).get_type(), Type::Normal);
+        assert_eq!(fs_wo(420636266).get_type(), Type::DNumber);
+        assert_eq!(fs_wo(701251144).get_type(), Type::DNumber);
+        assert_eq!(fs_wo(024636266).get_type(), Type::HNumber);
+        assert_eq!(fs_wo(305251144).get_type(), Type::HNumber);
+        assert_eq!(fs_wo(839184738).get_type(), Type::FHNumber);
+        assert_eq!(fs_wo(943598232).get_type(), Type::FHNumber);
     }
 }
